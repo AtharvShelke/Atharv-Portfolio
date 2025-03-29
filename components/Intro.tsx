b4e777873eec5db37813b5dd274a987deb711ebc
AtharvShelke <shelkeatharv964@gmail.com>
2025-03-29 19:01:08 +0530

    updated resume

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -77,10 +77,10 @@ const Intro = () => {
 
         <a
           className="border border-black/50 group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/Atharv_Shelke_Resume.pdf"
           download
         >
-          Download CV{" "}
+          Download Resume{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
